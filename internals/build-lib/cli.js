d2865859555dd7e45f7f057b8e072c4267275c94
Julien Papini <julien.papini@gmail.com>
2025-11-19 16:37:20 +0100

    feat(build-lib): add internal build lib script

---
+++ internals/build-lib/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import path from "node:path";
+
+import { build } from "tsdown";
+
+const cwd = process.cwd();
+
+await build({
+    cwd,
+
+    target: "node24",
+    platform: "node",
+
+    entry: path.resolve(cwd, "src/index.ts"),
+    outDir: path.resolve(cwd, "dist"),
+    tsconfig: path.resolve(cwd, "tsconfig.json"),
+
+    format: ["esm"],
+
+    dts: true,
+    sourcemap: true,
+    nodeProtocol: true,
+    shims: true,
+
+    watch: process.argv.includes("--watch"),
+    ignoreWatch: [/\/\.turbo\//u, /\/coverage\//u, /\/old\//u, /\/tmp\//u],
+});
