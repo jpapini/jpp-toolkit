cfc387a459ec1cd69c84a04c4c6d42221cc2df09
Julien Papini <julien.papini@gmail.com>
2025-12-06 19:04:34 +0100

    fix: git hooks install

--- packages/git-hooks/scripts/install.js
+++ packages/git-hooks/scripts/install.js
@@ -2,13 +2,14 @@
 import path from 'node:path';
 import url from 'node:url';
 
-import { findProjectRoot } from '@jpp-toolkit/utils';
 import husky from 'husky';
 
 if (process.env.CI !== undefined || process.env.NODE_ENV === 'production') {
     console.log('CI or production environment, skipping Husky installation.');
     process.exit(0);
 }
+
+const { findProjectRoot } = await import('@jpp-toolkit/utils');
 
 const currentDir = url.fileURLToPath(new URL('.', import.meta.url));
 const hooksDir = path.resolve(currentDir, '../hooks');
