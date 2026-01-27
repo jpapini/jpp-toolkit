e4b318099fc95721f7f9748fc9e1b39994f31581
Julien Papini <julien.papini@gmail.com>
2026-01-27 17:22:24 +0100

    feat: add asset rule regex

--- packages/rspack-config/src/constants.ts
+++ packages/rspack-config/src/constants.ts
@@ -1,4 +1,4 @@
 export const TS_RULE_TEST = /\.(?:ts|tsx|cts|mts)$/iu;
-export const ASSET_RULE_TEST = /\.(?:jpe?g|png|gif|svg)$/iu;
+export const ASSET_RULE_TEST = /\.(?:jpe?g|png|gif|svg|webp|avif|ico|bmp|tiff?|eot|ttf|woff2?)$/iu;
 export const CSS_RULE_TEST = /\.css$/iu;
 export const HASHED_JS_FILENAME_PATTERN = '[name].[contenthash:8].js';
